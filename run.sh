f61e68662f2f09aa7e9574b11b569a181c801358
Idinyte <black.mad.eagle@gmail.com>
2024-04-19 09:05:57 +0200

    add dataset

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
-datapath="$DATASET_DIR"
-datasets=('screw')
+datapath="dataset"
+datasets=('glassfiber')
 dataset_flags=($(for dataset in "${datasets[@]}"; do echo '-d '"${dataset}"; done))
 
 $CONDA_PREFIX/bin/python3.8 "$WORK_DIR"/SimpleNet_SDU/main.py
